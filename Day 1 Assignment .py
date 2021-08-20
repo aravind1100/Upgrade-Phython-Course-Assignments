65397010b150c3fdbae39be8d42617931cacf2ad
aravind1100 <89274763+aravind1100@users.noreply.github.com>
2021-08-21 01:27:53 +0530

    Add files via upload
    Finding power of a number

---
+++ Day 1 Assignment .py
@@ -0,0 +1,19 @@
+#Day 1 Assingnment, Phython course :
+	
+
+
+#Finding Exponent function :
+
+X = int(input("Enter a Base number : "))
+Y = int(input("Enter the power of a number : "))
+
+print("The answer is : ",pow(X,Y))
+
+
+
+
+'''
+For contact :
+Mail ID : aravindavd1999@gmail.com
+'''
+
